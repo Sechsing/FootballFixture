2f1f0d5e5b955772c6d8f50a66614b42820e6142
comtech2u <hd@comtech2u.com>
2024-11-25 13:44:48 +0800

    web-scraper4

--- web-scraper.ts
+++ web-scraper.ts
@@ -20,5 +20,5 @@ const scrapWebsite = async (url: string) => {
     }
 };
 
-const url = 'https://www.crossinghoods.com/software-engineer-test/)';
+const url = 'https://www.crossinghoods.com/software-engineer-test/';
 scrapWebsite(url);
