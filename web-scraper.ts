906af8cc4d35702c3f6b7712600a5e54903ca346
comtech2u <hd@comtech2u.com>
2024-11-25 11:20:35 +0800

    web-scraper3

--- web-scraper.ts
+++ web-scraper.ts
@@ -1,11 +1,24 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-const scrapWebsite = async (url: 'https://www.crossinghoods.com/software-engineer-test/)') => {
+const scrapWebsite = async (url: string) => {
     try {
         const { data } = await axios.get(url);
         const $ = cheerio.load(data);
         const title = $('title').text();
         console.log('Page Title:', title);
+
+        $('a').each((index, element) => {
+            const href = $(element).attr('href');
+            if (href) {
+              console.log(`Link ${index + 1}: ${href}`);
+            }
+          });
+      
+        } catch (error) {
+          console.error('Error scraping website:', error);
     }
-}
+};
+
+const url = 'https://www.crossinghoods.com/software-engineer-test/)';
+scrapWebsite(url);
